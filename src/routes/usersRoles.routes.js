ca3706386748ccce70c338699b978c62f88ebf6f
JuanaCelis02 <juana.celis@uptc.edu.co>
2023-11-15 15:15:37 -0500

    implementation api cache

--- src/routes/usersRoles.routes.js
+++ src/routes/usersRoles.routes.js
@@ -2,16 +2,18 @@ import { Router } from "express";
 import {getUsersRole, getRolesAndUsers, createUserRole, updateUserRoles, deleteUserRole, getUserWithRoles} from "../controllers/userRole.controller.js"
 import "../models/UserRole.js"
 import { authUser } from '../middlewares/login.middleware.js';
+import apicache from 'apicache'
 
 
 const router = Router()
+const cache = apicache.middleware
 
-router.get("/usersRoles", authUser, getUsersRole)
-router.get("/usersRoles/:id", authUser, getUserWithRoles)
-router.get("/rolesUsers", authUser, getRolesAndUsers)
-router.post("/userRoles", authUser, createUserRole)
-router.put("/userRoles", authUser, updateUserRoles)
-router.delete("/userRoles", authUser, deleteUserRole)
+router.get("/usersRoles", cache("1 minutes"), authUser, getUsersRole)
+router.get("/usersRoles/:id", cache("1 minutes"), authUser, getUserWithRoles)
+router.get("/rolesUsers", cache("1 minutes"), authUser, getRolesAndUsers)
+router.post("/userRoles", cache("1 minutes"), authUser, createUserRole)
+router.put("/userRoles", cache("1 minutes"), authUser, updateUserRoles)
+router.delete("/userRoles", cache("1 minutes"), authUser, deleteUserRole)
 
 
 export default router
